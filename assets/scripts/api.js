07d102bc7edb3ab539ad1d2836e067b628075434
Thaliaraujo <thalia.araujo321@gmail.com>
2024-01-03 13:14:35 -0300

    refactor: api language change

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,7 +1,7 @@
 const apiKey = 'c93c1f54b865a94eec32533acaccfb1e';
 
 export const getWeatherData = async(city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=en`;
 
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
@@ -11,7 +11,7 @@ export const getWeatherData = async(city) => {
 
 
 export const getForecastData = async (city) => {
-    const apiForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const apiForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}&lang=en`;
   
     const res = await fetch(apiForecastURL);
     const data = await res.json();
